4c4182b71bebb98fc39a5ea68d90dbe4e9bf84f0
Joko101002 <114925323+Joko101002@users.noreply.github.com>
2022-10-04 21:58:27 +0700

    Add files via upload

---
+++ latihan 1.php
@@ -0,0 +1,19 @@
+<?php
+//class mobile
+class mobile {
+    public $nama;
+    public $merk;
+
+    function getinfo () {
+        echo "Nama mobil:".$this->nama ."<br/>";
+        echo "Merk: ".$this->merk."<br/>";
+    }
+}
+
+//bagian main
+$ferari-new Mobil1();
+$ferari->nama-"xxx";
+$ferari->merk-"aaa";
+
+$ferari->getinfo();
+?>
